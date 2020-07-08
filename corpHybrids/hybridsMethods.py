af51f852bc49d19688c14bd77752434b57832b89
darren lefcoe <dlefcoe@gmail.com>
2020-07-08 21:32:07 +0100

    Add files via upload

---
+++ corpHybrids/hybridsMethods.py
@@ -0,0 +1,32 @@
+'''
+
+Module for corporate hybrid methods
+
+
+author:
+    @dlefcoe
+    darren@redhedge.uk
+
+
+'''
+
+
+
+
+
+def oneYearBonds(bondList):
+    ''' 
+    function to determine bonds that have 1 year to mature
+    
+        input: bondList, pandas dataframe containing bonds
+        
+        output: anotherList, a sorted list of bonds that will mature
+    
+    '''
+    # print(bondList)
+    print('the hybrids module has been invoked')
+    print('new things')
+    
+    return 0
+
+
